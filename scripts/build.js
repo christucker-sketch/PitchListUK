12dd697a52912d43cd5c2678bf1b32baa9d056d0
Chris Tucker <chris.tucker@normali.co.uk>
2026-04-30 13:56:03 +0000

    Add homepage imagery assets

--- scripts/build.js
+++ scripts/build.js
@@ -7,4 +7,12 @@ fs.mkdirSync(out, { recursive: true });
 for (const file of ['index.html', 'styles.css']) {
   fs.copyFileSync(path.join(root, 'src', file), path.join(out, file));
 }
+const assetsSrc = path.join(root, 'src', 'assets');
+const assetsOut = path.join(out, 'assets');
+if (fs.existsSync(assetsSrc)) {
+  fs.mkdirSync(assetsOut, { recursive: true });
+  for (const file of fs.readdirSync(assetsSrc)) {
+    fs.copyFileSync(path.join(assetsSrc, file), path.join(assetsOut, file));
+  }
+}
 console.log('Built public site to ./public');
