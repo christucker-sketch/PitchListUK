5e869e219aceb4fceca2156b3b7d979a2513dade
christucker-sketch <chris.tucker@normali.co.uk>
2026-08-27 15:00:07 +0100

    PLI-009 add pilot summary metadata

---
+++ operations/opportunity-pipeline/config/texas-pilot-summary.js
@@ -0,0 +1,6 @@
+export const TEXAS_PILOT_SUMMARY = Object.freeze({
+  live_first_party_sources_reviewed: 7,
+  approved_staging_sources: 6,
+  excluded_regulatory_sources: 1,
+  publication_enabled: false,
+});
